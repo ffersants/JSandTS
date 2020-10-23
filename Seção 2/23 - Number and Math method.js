effb2c408eb8226457611ef27671c155353aee78
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2020-10-23 01:05:43 -0300

    Math e number estudados.

---
+++ Seção 2/23 - Number and Math method.js
@@ -0,0 +1,22 @@
+let num1 = 10;
+
+let num2 = 51.49;
+
+//toFixed() determina o número de casas decimais a aparecerem após o ponto
+console.log(num2.toFixed(2));
+
+//eh inteiro ou não?
+console.log(Number.isInteger(num2))
+
+//Math.floor() arredonda para baixo
+console.log(Math.floor(num2));
+
+//Math.round() arredonda para onde tá mais perto
+console.log(Math.round(num2))
+
+//Math.ceil() arredonda para cima
+console.log(Math.ceil(num2))
+
+console.log(Math.min(50, 2, -156, 0, 5));
+
+console.log(Math.max(50, 2, -156, 0, 5));
