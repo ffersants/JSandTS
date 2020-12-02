d2e56e0aa3c76a972ff3928c4d93d14bd53a58b6
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2020-12-02 18:08:36 -0300

    Cronometro finalizado.

--- Seção 3/62 - Projeto cronometro/assets/js/main.js
+++ Seção 3/62 - Projeto cronometro/assets/js/main.js
@@ -1,6 +1,6 @@
 btns = document.getElementsByTagName("button");
 const relogio = document.getElementById("relogio");
-
+intervals = [];
 
 for (const btn of btns) {
   btn.addEventListener("click", (e) => {
@@ -8,14 +8,27 @@ for (const btn of btns) {
 
     switch (acao) {
       case 'iniciar' :
-        e.target.disabled = true  
-        iniciar();
+        e.target.disabled = true;
+        document.getElementById("pausar").disabled = false;
+        start = setInterval(iniciar, 1000);
+        intervals.push(start);
+        relogio.classList = "";
       break;
       case 'pausar' :
+        
+        if(document.getElementById("iniciar").disabled){
+          e.target.disabled = true;
+          document.getElementById("iniciar").disabled = false;
+          relogio.classList = 'pausado';
+        };
         pausar();
       break;
       case 'zerar' :
-        console.log('eh zerar')
+        document.getElementById("pausar").disabled = false;
+        document.getElementById("iniciar").disabled = false;
+        relogio.innerHTML = contador.reset();
+        relogio.classList = "";
+        pausar();
       break;
     }
   })
@@ -46,16 +59,23 @@ contador = {
     this.horas < 10 ? horasLong = '0' + this.horas : horasLong = this.horas;
 
     return `${horasLong}:${minutosLong}:${segundosLong}`;
+  },
+
+  reset(){
+    this.segundos = 0;
+    this.minutos = 0;
+    this.horas = 0;
+    return "00:00:00";
   }
 }
 
-function iniciar(){
-  setInterval(()=>{
-      contador.setSegundos()
-      relogio.textContent = contador.getTime()
-  }, 1000);
+function iniciar(){ 
+  contador.setSegundos()
+  relogio.textContent = contador.getTime()
 }
 
 function pausar(){
-  clearInterval(start)
+  intervals.forEach(i => {
+    clearInterval(i)
+  });
 }
