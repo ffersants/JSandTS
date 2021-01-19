fc4b50179b18d7eb9032c7a4241853c69c2cb614
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-01-19 06:21:06 -0300

    estudos promise encadeadas com catch/reject.

---
+++ Seção 8 - Assicronismo/Promises encadeadas e catch.js
@@ -0,0 +1,54 @@
+function doSomething() {
+
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            console.log('Primeiro isso');
+            resolve();
+        }, 5000)
+
+    })
+}
+
+doSomething()
+    .then(() => {
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log('Depois isso');
+                resolve()
+            }, 3000)
+        })
+
+    })
+    .then(() => {
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log('Por último isso');
+                try {
+                    lancarErro(5)
+                } catch (err) {
+                    reject(err)
+                } finally {
+                    resolve()
+                }
+            }, 8000)
+        })
+
+    })
+    .catch((err) => {
+        console.log('ops, teve erro ' + err);
+    })
+    .then(() => {
+        console.log('Execução finalizada')
+    })
+
+
+
+
+
+
+function lancarErro(num) {
+    if (typeof num === 'number') throw new Error('Isso é um numero')
+}
