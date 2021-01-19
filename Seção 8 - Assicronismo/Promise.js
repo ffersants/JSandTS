4f893804945eea3468500e4351f390069ae869d8
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-01-19 06:10:58 -0300

    Estudos com promises

--- Seção 8 - Assicronismo/Promise.js
+++ Seção 8 - Assicronismo/Promise.js
@@ -1,47 +1,65 @@
 {
-    function rand(min, max){
+    function rand(min, max) {
         min *= 1000;
-          max *= 1000;
-          return Math.floor(Math.random() * (max - min) + min)
-      }
-      
-      function esperaAi(msg, tempo){
+        max *= 1000;
+        return Math.floor(Math.random() * (max - min) + min)
+    }
+
+    function esperaAi(msg, tempo) {
         //Envolve-se na promise aquela função que sabemos que pode demorar a ser completada
-          return new Promise((resolve, reject) => {
-              setTimeout(() => {
-                  //console.log(msg)
-              }, tempo)
-          });
-      }
-      
-      esperaAi(`Vai demorar um pouco`, rand(1, 10))
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                //console.log(msg)
+            }, tempo)
+        });
+    }
+
+    esperaAi(`Vai demorar um pouco`, rand(1, 10))
 }
 
 {
-    function rand(min, max){
+    function rand(min, max) {
         min *= 1000;
-          max *= 1000;
-          return Math.floor(Math.random() * (max - min) + min)
-      }
-      
-      function esperaAi(msg, tempo){
-          return new Promise((resolve, reject) => {
-              setTimeout(() => {
-                  //chamará o then e passa como argumento a mensagem recebida pelo parametro em esperaAi
-                  resolve(msg)
+        max *= 1000;
+        return Math.floor(Math.random() * (max - min) + min)
+    }
+
+    function esperaAi(msg, tempo) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                //chamará o then e passa como argumento a mensagem recebida pelo parametro em esperaAi
+                resolve(msg)
                 //aqui a chamada do resolve demorará um tempo aleatório 
-              }, tempo)
-          });
-      }
-      
-      esperaAi(`Vai demorar um pouco`, rand(1, 10))
-          .then( mensagem => {
-              //e após esse tempo aleatório, será printado na tela a mensagem recebida
-              console.log(mensagem)
-              //é necessário retornar a função, que por sua vez retorna a promise que chamará o próximo then
-              return esperaAi(`Mensagem 1`, rand(1, 5))
-          })
-          .then( mensagem => {
+            }, tempo)
+        });
+    }
+
+    esperaAi(`Vai demorar um pouco`, rand(1, 10))
+        .then(mensagem => {
+            //e após esse tempo aleatório, será printado na tela a mensagem recebida
             console.log(mensagem)
-          })
+            //é necessário retornar a função, que por sua vez retorna a promise que chamará o próximo then
+            return esperaAi(`Mensagem 1`, rand(1, 5))
+        })
+        .then(mensagem => {
+            console.log(mensagem)
+        })
+}
+
+
+{
+    console.log('chegou aq')
+    demora = function () {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log('demora')
+                resolve()
+            }, 10000)
+        })
+    }
+
+    demora()
+        .then(() => {
+            console.log('eh isso ai')
+        })
 }
--- teste.js
+++ teste.js
@@ -1,7 +1,54 @@
-function primitiveMutator(val) {
-    val = val + 1;
-    console.log(val)
-  }
-  let x = 1;
-  primitiveMutator(x);
-  console.log(x); 
+function doSomething() {
+
+  return new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+      console.log('Primeiro isso');
+      resolve();
+    }, 5000)
+
+  })
+}
+
+doSomething()
+  .then(() => {
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log('Depois isso');
+        resolve()
+      }, 3000)
+    })
+
+  })
+  .then(() => {
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log('Por último isso');
+        try {
+          lancarErro(5)
+        } catch (err) {
+          reject(err)
+        } finally {
+          resolve()
+        }
+      }, 8000)
+    })
+
+  })
+  .catch((err) => {
+    console.log('ops, teve erro ' + err);
+  })
+  .then(() => {
+    console.log('Execução finalizada')
+  })
+
+
+
+
+
+
+function lancarErro(num) {
+  if (typeof num === 'number') throw new Error('Isso é um numero')
+}
