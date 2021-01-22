55fc6fc95374542e0cd51b7a8acea30cd7d816c0
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-01-22 07:14:06 -0300

    Async and await. Expression and function declaration.

--- Seção 8 - Assicronismo/106 - Async e await.js
+++ Seção 8 - Assicronismo/106 - Async e await.js
@@ -1,20 +1,46 @@
 function esperaAi(txt, time) {
     return new Promise((resolve, reject) => {
-        setTimeout((txt, time) => {
-            console.log(`isso demorará um pouquinho`)
+        console.log(`isso demorará um pouquinho`)
+        //erro será tratado dentro do bloco catch na função async
+        if (typeof txt !== 'string') reject('Bad value')
+        setTimeout(() => {
             resolve(txt)
         }, time * 1000)
-    }).then(t => console.log(t))
+    })
 }
-
+//uma declaração async function pode ter chamadas await de funções que retornam promises
 async function main() {
-    console.log('iniciando aqui')
-    await esperaAi('Promise 1 executada', 3)
-    console.log(`passando para a prox funcao`)
-    await esperaAi(`Promisse 2 executada`, 5)
-    console.log(`passando para a prox funcao`)
-    await esperaAi(`Promisse 3 executada`, 8)
-    console.log(`finalizado`)
+    //try = .then() ----- catch = .catch()
+    try {
+        promise1 = await esperaAi('Promise 1 executada', 2)
+        console.log(promise1)
+
+        promise2 = await esperaAi(`Promisse 2 executada`, 2)
+        console.log(promise2)
+
+        promise3 = await esperaAi(5, 2)
+        console.log(promise3)
+    } catch (e) {
+        console.error('Oops! ' + e)
+    }
 }
 
 main()
+
+//////////////ASYNC FUNCTION EXPRESSION//////////////
+{
+    function resolveAfter2Seconds(y) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(y)
+            }, 2000)
+        })
+    }
+
+    soma = async function (x) {
+        valor1 = await resolveAfter2Seconds(30)
+        valor2 = await resolveAfter2Seconds(20)
+        return valor1 + valor2 + x
+    }
+    soma(10).then(resultado => console.log(resultado))
+}
